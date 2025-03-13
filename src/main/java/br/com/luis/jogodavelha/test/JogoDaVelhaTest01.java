b2bc627ccf133559aecbe5f8035f3c37c82f823c
LuisFelipe7215 <afcgamerplays@gmail.com>
2025-03-13 11:32:17 -0300

    Criando a base do jogo

--- src/main/java/br/com/luis/jogodavelha/test/JogoDaVelhaTest01.java
+++ src/main/java/br/com/luis/jogodavelha/test/JogoDaVelhaTest01.java
@@ -1,23 +1,63 @@
 package br.com.luis.jogodavelha.test;
 
-import br.com.luis.jogodavelha.service.JogoDaVelhaService;
-
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class JogoDaVelhaTest01 {
-
-    private static JogoDaVelhaService jogoDaVelhaService = new JogoDaVelhaService();
+    private static char[][] tabuleiro = new char[3][3];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        opcaoJogador(sc);
+        iniciaJogo();
+        char simboloJogador = simboloJogador();
 
+        boolean fimDoJogo = false;
+        int linha, coluna;
+        int jogador = 1;
+        while (!fimDoJogo) {
+            jogador = jogador % 2 == 0 ? 2 : 1;
+            System.out.printf("Vez do jogador %d com o símbolo '%c'%n", jogador++, simboloJogador);
+            System.out.printf("Insira a posição da linha do %c (1 até 3)%n", simboloJogador);
+            linha = sc.nextInt() - 1;
+            System.out.printf("Insira a posição da coluna do %c (1 até 3)%n", simboloJogador);
+            coluna = sc.nextInt() - 1;
+
+            adiciona(linha, coluna, simboloJogador);
+
+            simboloJogador = simboloJogador == 'X' ? 'O' : 'X';
+        }
     }
 
-    private static void opcaoJogador(Scanner sc) {
-        System.out.println("Escolha uma opção de letra para o jogador 1:");
-        System.out.println("1. X");
-        System.out.println("2. O");
-        jogoDaVelhaService.opcaoJogador(sc.nextInt());
+    private static void adiciona(int linha, int coluna, char simboloJogador) {
+        tabuleiro[linha][coluna] = simboloJogador;
+        imprime();
     }
+
+    private static char simboloJogador() {
+        System.out.println("O primeiro símbolo será sorteado aleatoriamente (X ou O)");
+        char simbolo = ThreadLocalRandom.current().nextInt(0, 2) == 1 ? 'X' : 'O';
+        System.out.printf("O símbolo do jogador 1 é o %c%n", simbolo);
+        System.out.printf("O símbolo do jogador 2 é o %c%n", simbolo == 'X' ? 'O' : 'X');
+
+        return simbolo;
+    }
+
+
+    private static void iniciaJogo() {
+        for (char[] chars : tabuleiro) {
+            Arrays.fill(chars, ' ');
+        }
+    }
+
+    private static void imprime() {
+        for (char[] simbolos : tabuleiro) {
+            for (char simbolo : simbolos) {
+                System.out.printf("|%c|", simbolo);
+            }
+            System.out.println();
+        }
+    }
+
+
 }
